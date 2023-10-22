9b7ef9feb664a5835489fc352228bef28397b917
Ra <rami@jwcorp.fr>
2023-10-22 11:01:30 +0200

    added compliance request fetch + node-fetch and added access token

--- sendMessage.js
+++ sendMessage.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const axios = require('axios');
 
 async function sendMessage() {
+  // for Create a new message
   const url = 'https://api.linkedin.com/v2/messages';
   const data = {
     recipients: ['urn:li:person:123ABC', 'urn:li:person:456DEF'],
@@ -10,13 +11,16 @@ async function sendMessage() {
     messageType: 'MEMBER_TO_MEMBER',
     attachments: ['urn:li:digitalMediaAsset:123ABC']
   };
+  // for repl
   const config = {
     headers: {
-      Authorization: `Bearer ${process?.env?.LINKEDIN_CLIENT_SECRET}`,
+      // Authorization: `Bearer ${process?.env?.LINKEDIN_CLIENT_SECRET}`,
+      // Authorization: `Bearer ${process?.env?.REFRESH_TOKEN}`,
+      Authorization: `Bearer ${process?.env?.ACCESS_TOKEN}`,
       'Content-Type': 'application/json'
     }
   };
-  console.log(config?.headers)
+  // console.log(config?.headers?.Authorization)
   try {
     const response = await axios.post(url, data, config);
     console.log(response.data);
@@ -24,5 +28,19 @@ async function sendMessage() {
     console.error(error);
   }
 }
-
-sendMessage();
+async function memberRegulation () {
+  try {
+    const response = await axios.get("https://api.linkedin.com/v2/memberComplianceAuthorizations?q=member", {
+      headers: {
+        Authorization: `Bearer ${process?.env?.ACCESS_TOKEN}`
+      }
+    });
+    // Traitement de la réponse
+    console.log(response.data);
+  } catch (error) {
+    // Gestion des erreurs
+    console.error("Une erreur est survenue:", error);
+  }
+}
+// sendMessage();
+memberRegulation();
